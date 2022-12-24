9a27d02d0a9dea79a142c07ba615f39b0b805e1f
mannu shekhawat <115095365+Shekhawatmannu382@users.noreply.github.com>
2022-12-24 13:33:13 +0530

    Add files via upload

---
+++ login/script.js
@@ -0,0 +1,28 @@
+const inputEmail = document.getElementById('input-email');
+const inputPassword = document.getElementById('input-password');
+const emailWarning = document.getElementById('input-email-warning');
+const passWarning = document.getElementById('input-password-warning');
+
+function checkForErrors (inputValue, warningElement) {
+let error = false;
+if (inputValue === '') {
+warningElement.innerHTML = 'This field is required';
+error = true;
+} else if (inputValue.length < 3) {
+warningElement.innerHTML = 'Must be at least 3 character';
+
+error = true;
+} else {
+warningElement.innerHTML='';
+}
+return error;
+}
+
+function login() {
+const emailError = checkForErrors(inputEmail.value, emailWarning);
+const passError = checkForErrors(inputPassword.value, passWarning);
+if(!emailError && !passError) {
+location.href = location.href + 'success.html';
+}
+}
+    
